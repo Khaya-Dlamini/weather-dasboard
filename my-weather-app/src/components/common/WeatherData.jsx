353cc48d12d27d7a90c37151f4e7a77e4f1d693b
Khayelihle <khayadlamini563@gmail.com>
2024-10-09 18:55:45 +0200

    added the weatherdata component

---
+++ my-weather-app/src/components/common/WeatherData.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function WeatherData() {
+  return (
+    <div className="weather">
+        <i class="bi bi-brightness-high-fill"></i>
+        <div className="weather type">Clear</div>
+        <div className="temp">28˚</div>
+        <div className="weather-date">
+            <p>Fri,3 May</p>
+        </div>
+        <div className="weather-data">
+            <div className="humidity">
+                <div className="data-name">Humidity</div>
+                <i class="bi bi-droplet"></i>
+                <div className="data">35%</div>
+            </div>
+            <div className="wind">
+                <div className="data-name">Wind</div>
+                <i class="bi bi-wind"></i>
+                <div className="data">3 km/h</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WeatherData
--- my-weather-app/src/components/pages/WeatherCard.jsx
+++ my-weather-app/src/components/pages/WeatherCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SearchBar from '../common/SearchBar'
+import WeatherData from '../common/WeatherData'
 function WeatherCard() {
   return (
     <div className='containerss'>
     <div className='weather-app'>
         <SearchBar/>
-        
+        <WeatherData/>
     </div>
         </div>  )
 }
