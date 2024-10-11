6e118d02b647692a697a50e768dedfb8dd363677
Khayelihle <khayadlamini563@gmail.com>
2024-10-11 18:31:20 +0200

    added background colors and styled the search container

--- my-weather-app/src/components/common/SearchBar.jsx
+++ my-weather-app/src/components/common/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchBar() {
   return (
     <div className='search'>
-    <div className='search-topss'>
+    <div className='search-top'>
     <i class="bi bi-geo-alt-fill"></i>
     <div className='location'>London</div>
     </div>
